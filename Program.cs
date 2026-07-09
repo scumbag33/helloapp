c62f4453eddf0364924b2dd4d4773adbdcc8e184
white <white@example.com>
2026-07-09 21:45:42 +0500

    Add GreetBlack function

--- Program.cs
+++ Program.cs
@@ -15,3 +15,14 @@ static void greetBlack()
 {
     Console.WriteLine("hello black");
 }
+Console.WriteLine("hello everyone");
+
+static void GreetWhite()
+{
+    Console.WriteLine("hello white");
+}
+
+static void GreetBlack()
+{
+    Console.WriteLine("hello black");
+}
